5f896055b4eba9a1566e661a5243ad6de68842fc
MuhammadOsama169 <m.osama169@gmail.com>
2023-10-04 14:18:18 +0300

    feat: add JWT strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @darraghor/nestjs-typed/injectable-should-be-provided */
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config:ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
