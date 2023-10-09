9c8ad4eac0f46fbdda72203805a1e889bd2f0edf
MuhammadOsama169 <m.osama169@gmail.com>
2023-10-09 12:28:01 +0300

    feat: add test db and setup tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,3 +1,28 @@
+import {Test} from '@nestjs/testing'
+import {INestApplication,ValidationPipe} from '@nestjs/common'
+import { AppModule } from '../src/app.module'
+
 describe('App end2end',()=>{
+  let app: INestApplication
+  //starting logic
+  beforeAll(async () => {
+    const moduleRef =
+      await Test.createTestingModule({
+        imports:[AppModule],
+      }).compile()
+      const app = moduleRef.createNestApplication();
+
+      app.useGlobalPipes(
+        // eslint-disable-next-line @darraghor/nestjs-typed/should-specify-forbid-unknown-values
+        new ValidationPipe ({
+          whitelist:true,
+        })
+      )
+      await app.init()
+  })
+  //teardown logic
+  afterAll(()=>{
+    app.close()
+  })
   it.todo('should pass')
 })
